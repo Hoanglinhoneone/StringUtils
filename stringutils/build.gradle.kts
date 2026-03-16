b2cd637cd950cc9cdcbfda844cfd6e5e4005febb
hoanglinhoneone <hoangngoclinh305@gmail.com>
2026-03-16 23:24:12 +0700

    feat: update SDK v1.0.1

--- stringutils/build.gradle.kts
+++ stringutils/build.gradle.kts
@@ -63,7 +63,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.YOUR_GITHUB_USERNAME"
                 artifactId = "StringUtilsSDK"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
